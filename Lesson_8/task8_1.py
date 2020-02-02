a17a55733397c86a87bb81cce2ce099e4551ec23
Solda-git <soldatovsl@mail.ru>
2020-02-02 22:39:40 +0300

    Homework 8 task 1 Haffman task fully completed

--- Lesson_8/task8_1.py
+++ Lesson_8/task8_1.py
@@ -4,6 +4,9 @@
 from collections import Counter, deque
 
 class TreeElement:
+    """
+    Service class for Tree, provides Tree Node elements
+    """
     def __init__(self, node_value=None, node_weight=0):
         self.value = node_value
         self.weight = node_weight
@@ -13,9 +16,11 @@ class TreeElement:
         #self.index = 0
 
 class Tree:
-
+    """
+    Service Class for Haffman class, provides Tree structure for coding
+    """
     def __init__(self, tree_elem):
-        self.elems=[]
+        self.elems = []
         self.elems.append(tree_elem)
 
     def __str__(self):
@@ -28,6 +33,13 @@ class Tree:
     #    return self[0].weight
 
     def build_branch(self, left_node, right_node):
+        """
+        function builds up tree structure using self.node as root,
+        left_node as left branch, right_node as right branch
+        :param left_node:
+        :param right_node:
+        :return:
+        """
         if len(self.elems) > 1:
             print('Operation is valir for singe node tree only')
             return self
@@ -49,17 +61,23 @@ class Haffman:
         self.message_text = input_string
         self.freq_table = Counter(input_string)
         self.code_dict = {}
+        self.haffman_tree = Tree(TreeElement())
         #serv_deque = deque(sorted(self.freq_table.items(), key = lambda i: i[1]))
         #print(serv_deque)
 
     def build_tree(self):
+        """
+        function builds binary tree
+        :return: tree link
+        """
         # forming of ascending sorted deque of Trees with single node (root)
         # from the symbols frequency table saved in Counter(freq_table)
-        tdeque = deque(sorted([Tree(TreeElement(i[0], self.freq_table[i[0]])) for i in self.freq_table], key=lambda x: x.elems[0].weight))
+        tdeque = deque(sorted([Tree(TreeElement(i[0],\
+        self.freq_table[i[0]])) for i in self.freq_table], key=lambda x: x.elems[0].weight))
         while len(tdeque) > 2:
-            print(f"len(tdeque): {len(tdeque)}")
+            #print(f"len(tdeque): {len(tdeque)}")
             left_tree = tdeque.popleft()
-            print(left_tree)
+            #print(left_tree)
             right_tree = tdeque.popleft()
             new_tree = Tree(TreeElement())
             new_tree.build_branch(left_tree, right_tree)
@@ -69,23 +87,19 @@ class Haffman:
                     break
                 if i == len(tdeque)-1:
                     tdeque.append(new_tree)
-        self.haffman_tree = Tree(TreeElement())
+        #self.haffman_tree = Tree(TreeElement())
         left_tree = tdeque.popleft()
         right_tree = tdeque.popleft()
         self.haffman_tree.build_branch(left_tree, right_tree)
 
-    def build_code_tab(self, node = None, code_str=""):
-
-        """ for i, item in enumerate(self.haffman_tree.elems):
-            if item.value is not None:
-                reverse_code=''
-                while item.parent is not None:
-                    if item.parent.left == item:
-                        reverse_code += '0'
-                    else:
-                        reverse_code += '1'
-                self.code_dict[item.value] = reverse_code.reverse()
+    def build_code_tab(self, node=None, code_str=""):
         """
+        function builds Haffman code table
+        :param node: current tree node
+        :param code_str: current code string
+        :return: dictionary of  Haffman code table
+        """
+
         #if first call without 'node' parameter
         if node is None:
             node = self.haffman_tree.elems[0]
@@ -96,49 +110,15 @@ class Haffman:
             self.build_code_tab(node.right, code_str + "1")
         return self.code_dict
 
-    def encript_message(self):
-        pass
+    def encrypt_message(self):
+        """
+        function returns encrypted message
+        :return:
+        """
+        return ''.join([self.code_dict[i] for i in self.message_text]) #result
 
-    def __str__(self):
-        return self.encripted
-
-
-test = Haffman('beep boop beer!')
-test.build_tree()
-print(test.build_code_tab())
-
-
-## formed tree testing
-
-
-print(str(test.haffman_tree.elems[0].left.left.value))
-print("!!! ", str(test.haffman_tree.elems[0].left.right.right.value), " !!!")
-
-print(str(test.haffman_tree.elems[0].right.right.value))
-print(str(test.haffman_tree.elems[0].right.left.left.right.value))
-
-#print(test.freq_table)
-"""
-tr1 =  Tree(TreeElement())
-tr2 =  Tree(TreeElement('r', 1))
-tr3 =  Tree(TreeElement('!', 1))
-tr4 =  Tree(TreeElement('p', 2))
-tr5 =  Tree(TreeElement())
-
-tr1.build_branch(tr2,tr3)
-tr5.build_branch(tr1,tr4)
-
-print(tr5)
-print(tr5.elems[0].left.right.value)
-#freq_table = Counter("beep boop beer!")
-#list = [freq_table[i[0]] for i in freq_table ]
-#tlist = deque([Tree(TreeElement(i[0],freq_table[i[0]])) for i in freq_table ])
-
-#tlist = deque(sorted([Tree(TreeElement(i[0],freq_table[i[0]])) for i in freq_table], key=lambda x: x.elems[0].weight))
-
-#sorted("beep boop beer!")
-
-#print(freq_table)
-#print(f"Первый символ: {tlist[0].elems[0].value}, частота: {tlist[0].elems[0].weight}.")
-#print(f"Последний символ: {tlist[len(tlist)-1].elems[0].value}, частота: {tlist[len(tlist)-1].elems[0].weight}.")
-"""
+MY_HAFFMAN = Haffman('beep boop beer!')
+MY_HAFFMAN.build_tree()
+print("Test message: ", MY_HAFFMAN.message_text)
+print("Code table: ", MY_HAFFMAN.build_code_tab())
+print("Encripted message: ", MY_HAFFMAN.encrypt_message())
